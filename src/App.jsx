929e8a28c49e93d9145bfe9694295032076d6ea6
Geno <davidgeno55@gmail.com>
2023-06-25 01:31:39 +0530

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
-import { FaSearchLocation } from "react-icons/fa";
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [weather, setWeather] = useState([])
   const [search, setSearch] = useState('')
+  console.log(weather);
 
   useEffect(() => {
     data()
@@ -21,50 +20,35 @@ function App() {
     const store = e.target.value
     setSearch(store)
   }
-  const handlesubmit = () => {
-    if (search !== '') {
-      data()
-    }
-  }
-  console.log(weather);
 
   return (
-    <>
-      {/* <button onClick={() => { setCount(count + 1) }}>+</button>
-      {count}
-      <button disabled={count === 0 ? true : false} onClick={() => { setCount(count <= 0 ? 0 : count - 1) }}>-</button > */}
-      <div className='container'>
-        <div className='weather'>
-          <h1 className='heading'>Location Informations</h1>
-          <div className='search'>
-            <input className='search-input' onChange={(e) => handlesearch(e)} type='text' placeholder='Enter the city name' />
-            <button onSubmit={() => handlesubmit()} className='search-btn' type='submit'>
-              <FaSearchLocation />
-            </button>
-          </div>
-          <div className="weatherinfo">
-            {
-              weather.map((value, key) => {
-                return (
-                  <>
-                    <div className='info'>
-                      <h1>{value.clouds}°F</h1>
-                      <h2>{value.city_name}</h2>
-                      <div className='detail'>
-                        <p> Sunrise={value.sunrise}</p>
-                        <p>{value.wind_cdir_full}</p>
-                        <p>Sunset={value.sunset}</p>
-                      </div>
+    <div className='container'>
+      <div className='weather'>
+        <h1 className='heading'>Location Informations</h1>
+        <div className='search'>
+          <input className='search-input' onChange={(e) => handlesearch(e)} type='text' placeholder='Enter the city name' />
+        </div>
+        <div className="weatherinfo">
+          {
+            weather.map((value, key) => {
+              return (
+                <>
+                  <div className='info'>
+                    <h1>{value.clouds}°F</h1>
+                    <h2>{value.city_name}</h2>
+                    <div className='detail'>
+                      <p> Sunrise={value.sunrise}</p>
+                      <p>{value.wind_cdir_full}</p>
+                      <p>Sunset={value.sunset}</p>
                     </div>
-                  </>
-                )
-              })
-            }
-          </div>
+                  </div>
+                </>
+              )
+            })
+          }
         </div>
       </div>
-
-    </>
+    </div>
   )
 }
 
